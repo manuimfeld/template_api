bf349badde61d68aaf2f7c9a6c5594ea1f462d83
Manuimfeld <manu_jadir@hotmail.com>
2023-07-03 03:28:45 -0300

    Edit model user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  username: { type: String, unique: true, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "user" },
-  date: { type: Date, default: Date.now },
-});
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "user" },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 export const User = mongoose.model("Users", userSchema);
